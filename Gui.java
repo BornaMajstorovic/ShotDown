001dbea754d760b954859800b79929a8448ade72
BornaMajstorovic <48225205+BornaMajstorovic@users.noreply.github.com>
2019-05-03 14:04:57 +0200

    Add files via upload

---
+++ Gui.java
@@ -0,0 +1,64 @@
+package ShutTheFuckDown;
+
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.ActionEvent;
+
+import javax.swing.*;
+
+public class Gui extends JFrame {
+	
+	
+	public Gui() {
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setBounds(1250, 550, 600, 600);
+		setTitle("Shot down");
+		
+		
+		JPanel panel = (JPanel) getContentPane();
+		panel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		
+		ImageIcon image = new ImageIcon("night.jpg");
+		JLabel label = new JLabel(image);
+		panel.add(label,BorderLayout.CENTER);
+		
+	
+		
+		JLabel time = new JLabel("Number of minutes before shotdown");
+		panel.add(time,BorderLayout.WEST);
+		
+		final JTextField input = new JTextField();
+		panel.add(input, BorderLayout.CENTER);
+		input.setColumns(5);
+		
+		
+		JPanel southPanel = new JPanel();
+		panel.add(southPanel,BorderLayout.SOUTH);
+		
+		JButton go = new JButton("GO");
+		southPanel.add(go);
+		
+		go.addActionListener(new ActionListener(){
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Main main = new Main();
+				int mins = Integer.parseInt(input.getText());
+				main.go(mins);
+				JOptionPane.showMessageDialog(Gui.this, "Shutting down in " + mins + " minutes");
+				
+			}
+			});
+	}
+	
+	public static void main(String[] args) throws Exception{
+		SwingUtilities.invokeAndWait(()->{
+			Gui window = new Gui();
+			window.pack();
+			window.setVisible(true);
+			});
+	}
+
+}
---
+++ Main.java
@@ -0,0 +1,34 @@
+package ShutTheFuckDown;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+
+public class Main {
+
+	
+	public void go(Integer min) {
+		
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			
+			@Override
+			public void run() {
+				ProcessBuilder processBuilder = new ProcessBuilder("shutdown", "/s");
+				try {
+					processBuilder.start();
+				}catch(IOException e) {
+					throw new RuntimeException(e);
+				}
+				
+			}
+			
+			},min*60*1000);
+		
+		System.out.println("Shutting down in " + min + " minutes");
+		
+	}
+
+}
